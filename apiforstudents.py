a416770c8234575b75858a2aec692ca17fc5195c
സേ വ്യ ർ <sebastian_xavier@outlook.com>
2025-04-08 14:30:46 -0500

    refactor: apply title case to names in the DataFrame api for students

--- apiforstudents.py
+++ apiforstudents.py
@@ -6,6 +6,29 @@ EXCEL_PATH = "/Users/divakaryadav/Documents/SRPC_DATA/2025rpc.xlsx"
 API_URL = "https://api.uwmsrpc.com/api/home/students/create"
 USE_PROD = True
 TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQ0MTA3MzI4LCJpYXQiOjE3NDQwODkzMjgsImp0aSI6IjcwMWViODc4ZDJhMDQ4NGJiNmMzODUzZjM4NTIyZDI5IiwidXNlcl9pZCI6M30.RV2laLadhmaosVQVMyPavcaKmHmw1K89ogmVhimO8mo"
+def sentence_case(text):
+    # "MY RESEARCH POSTER" -> "My research poster"
+    return text.capitalize() if isinstance(text, str) else text
+
+def title_case(text):
+    #  "jOHN doe" -> "John Doe"
+    return text.title() if isinstance(text, str) else text
+
+def lower_case(text):
+    # "JOHN.DOE@UWM.EDU" -> "john.doe@uwm.edu"
+    return text.lower() if isinstance(text, str) else text
+for col in ["First Name", "Last Name", "Phonetic spelling", "Research adviser first name", "Research adviser last name"]:
+    if col in df.columns:
+        df[col] = df[col].apply(title_case)  # E.g. "aLIce" -> "Alice"
+
+if "Title" in df.columns:
+    df["Title"] = df["Title"].apply(sentence_case)  #  "QUANTUM CIRCUIT DESIGN" -> "Quantum circuit design"
+
+if "email" in df.columns:
+    df["email"] = df["email"].apply(lower_case)
+
+if "Research adviser email" in df.columns:
+    df["Research adviser email"] = df["Research adviser email"].apply(lower_case)
 
 df = pd.read_excel(EXCEL_PATH).fillna("")
 
