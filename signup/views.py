07c923c9af675cec761bd7c85702efe8542b5a8b
സേ വ്യ ർ <sebastian_xavier@outlook.com>
2025-04-10 17:21:24 -0500

    Update views.py
    added date for reg dead line

--- signup/views.py
+++ signup/views.py
@@ -7,13 +7,17 @@ from rest_framework import status
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from django.contrib.auth.models import User
-
-
+from datetime import datetime
+import pytz
+chicago_tz = pytz.timezone('America/Chicago')
+REG_DEADLINE = chicago_tz.localize(datetime(2025, 4, 25, 9, 0))  # for managing registration deadline 9:00 AM CST on April 10, 2025
 class Signup(APIView):
 
     renderer_classes = [renderers.JSONRenderer]
 
     def post(self, request):
+        #if datetime.now().astimezone(chicago_tz) > REG_DEADLINE:
+            #return Response({'success': False, 'message': 'Registration is closed.'}, status=status.HTTP_403_FORBIDDEN)
         print(request.data)
 
         serializer = UserSerializer(data=request.data)
