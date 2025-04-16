4cea790c8635fcd36b9fb7d2a0dde7c5854834e7
സേ വ്യ ർ <sebastian_xavier@outlook.com>
2025-04-16 15:48:14 -0500

    fix: update registration deadline to April 17, 2025 11 am

--- signup/views.py
+++ signup/views.py
@@ -10,14 +10,14 @@ from django.contrib.auth.models import User
 from datetime import datetime
 import pytz
 chicago_tz = pytz.timezone('America/Chicago')
-REG_DEADLINE = chicago_tz.localize(datetime(2025, 4, 25, 9, 0))  # for managing registration deadline 9:00 AM CST on April 10, 2025
+REG_DEADLINE = chicago_tz.localize(datetime(2025, 4, 17, 11, 0))  # for managing registration deadline 9:00 AM CST on April 10, 2025
 class Signup(APIView):
 
     renderer_classes = [renderers.JSONRenderer]
 
     def post(self, request):
-        #if datetime.now().astimezone(chicago_tz) > REG_DEADLINE:
-            #return Response({'success': False, 'message': 'Registration is closed.'}, status=status.HTTP_403_FORBIDDEN)
+        if datetime.now().astimezone(chicago_tz) > REG_DEADLINE:
+            return Response({'success': False, 'message': 'Registration is closed.'}, status=status.HTTP_403_FORBIDDEN)
         print(request.data)
 
         serializer = UserSerializer(data=request.data)
