eb7dce442aa89b8702bca419a56303599be44741
സേ വ്യ ർ <sebastian_xavier@outlook.com>
2025-04-24 16:32:55 -0500

    feat: implement judge registration script with Excel integration and deadline check

--- signup/views.py
+++ signup/views.py
@@ -10,7 +10,7 @@ from django.contrib.auth.models import User
 from datetime import datetime
 import pytz
 chicago_tz = pytz.timezone('America/Chicago')
-REG_DEADLINE = chicago_tz.localize(datetime(2025, 4, 17, 11, 0))  # for managing registration deadline 9:00 AM CST on April 10, 2025
+REG_DEADLINE = chicago_tz.localize(datetime(2025, 4, 26, 11, 0))  # for managing registration deadline 9:00 AM CST on April 10, 2025
 class Signup(APIView):
 
     renderer_classes = [renderers.JSONRenderer]
@@ -34,3 +34,5 @@ class Signup(APIView):
             # to the user
             print(serializer.errors)
             return Response({'success': False, 'message': 'User registration failed.', 'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
